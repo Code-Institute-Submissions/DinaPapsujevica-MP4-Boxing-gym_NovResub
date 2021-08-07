abc6835b2e5551202a7181d579c6ff078db28cb7
DinaPapsujevica <dina.papsujevica@gmail.com>
2021-08-07 15:06:21 +0000

    Created product template

--- home/views.py
+++ home/views.py
@@ -12,3 +12,9 @@ def fitnessplans(request):
     """ A view to return the fitnessplans page """
 
     return render(request, 'home/fitnessplans.html')
+
+
+def products(request):
+    """ A view to show all products """
+
+    return render(request, 'home/products.html')
