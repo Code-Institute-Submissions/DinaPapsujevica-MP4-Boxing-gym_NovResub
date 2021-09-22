3efbcf769c582a58dee676309bf00a17c0993ca8
DinaPapsujevica <ranjeet>
2021-09-22 22:31:19 +0545

    dotenv libary added to requirement

--- boxing_gym/settings.py
+++ boxing_gym/settings.py
@@ -104,12 +104,8 @@ WSGI_APPLICATION = 'boxing_gym.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DATABASE_NAME"),
-        'USER': os.getenv("DATABASE_USER"),
-        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
-        'HOST': os.getenv("DATABASE_HOST"),
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 WHITENOISE_USE_FINDERS = True
